77361193f6607360c5a78e337d5983759e7d340c
Peter Johnson <peter.johnson@relativity.com>
2022-01-12 16:58:52 -0600

    added tf file and apply task

---
+++ Terraform/djpythonapp1.tf
@@ -0,0 +1,23 @@
+resource "azurerm_resource_group" "djpythonapp1" {
+    name = "djpythonapp1-test"
+    location = "Central US"
+}
+
+resource "azurerm_app_service_plan" "djpythonapp1" {
+    name = "djpythonapp1-test"
+    location = "Central US"
+    resource_group_name = azurerm_resource_group.djpythonapp1.name
+    kind = "Linux"
+    reserved = false
+
+    zone_redundant = false
+    per_site_scaling = false
+    
+
+    sku {
+      tier = "Free"
+      sku = "F1"
+      capacity = 1
+    }
+
+}
