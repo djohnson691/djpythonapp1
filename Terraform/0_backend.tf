b35b2832a5c6d5e32cb4b61bfff3ffba6c1103fd
Peter Johnson <peter.johnson@relativity.com>
2022-01-12 17:02:56 -0600

    added workingdir

--- Terraform/0_backend.tf
+++ Terraform/0_backend.tf
@@ -1,3 +1,4 @@
+/*
 terraform {
   backend "azurerm" {
     storage_account_name = "djpythonapp1testtfstate"
@@ -9,5 +10,6 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = "c0e81386-05f1-4c94-b8e3-51b0bc486450"
-  version         = ">= 2.0"
+  #version         = ">= 2.0"
 }
+*/
