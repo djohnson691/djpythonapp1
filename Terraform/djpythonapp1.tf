c2d44942aaef7c890800272aa9e5b01864b54dfe
Peter Johnson <peter.johnson@relativity.com>
2022-01-13 13:53:25 -0600

    added azurerm_app_service and azurerm_application_insights

--- Terraform/djpythonapp1.tf
+++ Terraform/djpythonapp1.tf
@@ -7,25 +7,44 @@ terraform {
 }
 
 resource "azurerm_resource_group" "djpythonapp1" {
-    name = "test"
-    location = "Central US"
+  name     = "test"
+  location = "Central US"
 }
 
 resource "azurerm_app_service_plan" "djpythonapp1" {
-    name = "djpythonapp1-test"
-    location = "Central US"
-    resource_group_name = azurerm_resource_group.djpythonapp1.name
-    kind = "Linux"
-    reserved = true
+  name                = "djpythonapp1-test"
+  location            = "Central US"
+  resource_group_name = azurerm_resource_group.djpythonapp1.name
+  kind                = "Linux"
+  reserved            = true
 
-    zone_redundant = false
-    per_site_scaling = false
-    
+  zone_redundant   = false
+  per_site_scaling = false
 
-    sku {
-      tier = "Free"
-      size = "F1"
-      capacity = 1
-    }
 
+  sku {
+    tier     = "Free"
+    size     = "F1"
+    capacity = 1
+  }
+
+}
+
+resource "azurerm_application_insights" "djpythonapp1" {
+  name                = "djpythonapp1-test"
+  location            = "Central US"
+  resource_group_name = azurerm_resource_group.djpythonapp1.name
+}
+
+resource "azurerm_app_service" "djpythonapp1" {
+  name                = "djpythonapp1-test"
+  location            = "Central US"
+  resource_group_name = azurerm_resource_group.djpythonapp1.name
+  app_service_plan_id = azurerm_app_service_plan.djpythonapp1.id
+  site_config {
+    linux_fx_version = "PYTHON|3.9"
+  }
+  app_settings = {
+    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.djpythonapp1.instrumentation_key
+  }
 }
